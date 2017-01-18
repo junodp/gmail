9b86b157c3e3ce32fa9df294eede8a5fe167c24b
junodp <herjunodaudpramono@gmail.com>
2017-01-18 11:30:41 +0700

    Update config.php

--- config.php
+++ config.php
@@ -4,11 +4,12 @@
 
 $scope = "https://mail.google.com/"; // Do not change it!
 
-$redirect_uri = "http://localhost/gmail/sign-in.php"; // Enter your redirect_uri
+//$redirect_uri = "http://localhost/gmail/sign-in.php"; // Enter your redirect_uri
 
-$client_id = "429600818916-8865m2c7rqj8rk6jftkcr2ek7fli14mu.apps.googleusercontent.com"; // Enter your client_id
+//$client_id = "429600818916-8865m2c7rqj8rk6jftkcr2ek7fli14mu.apps.googleusercontent.com"; // Enter your client_id
 
-$client_secret = "1So_CEvYDtVZ8DHRfXkLq129"; // Enter your client_secret
+//$client_secret = "1So_CEvYDtVZ8DHRfXkLq129"; // Enter your client_secret
+<script src = " AIzaSyCnts7lkZESlyyDqbfL003occfI4OCo8DE" dever></script>
 
 $login_url = "https://accounts.google.com/o/oauth2/v2/auth?scope=$scope&response_type=code&redirect_uri=$redirect_uri&client_id=$client_id"; // Do not change it!
 
